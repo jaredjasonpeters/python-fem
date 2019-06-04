a200e0ea4a7fcd2866b2461f1ae661ed88f18899
Jared J. Peters <jjpeters@dlfna.com>
2019-06-03 21:35:26 -0700

    initial commit

---
+++ name_lib.py
@@ -0,0 +1,10 @@
+def upper_case_name(name):
+    return name.upper()
+
+
+if __name__ == "__main__":
+
+    name = "Jared"
+    upper_case = upper_case_name(name)
+    print(f'Upper case name is {upper_case}')
+    print(__name__)
---
+++ project.py
@@ -0,0 +1,8 @@
+greetings = ["Hello", "Bonjour", "Hola"]
+
+# for greeting in greetings:
+#     print(f"{greeting}, World!")
+
+greeting_by_region = [print(f'{greeting} World!') for greeting in greetings]
+
+greeting_by_region
